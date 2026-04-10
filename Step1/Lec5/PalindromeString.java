75b333be0ffb21497fac67983b43f18acab07175
Vinay Kumar J <jvkumar2003@gmail.com>
2026-04-10 19:40:29 +0530

    String Palindrome Check

---
+++ Step1/Lec5/PalindromeString.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+
+    public static boolean  isPalindrome(String str , int s, int e) {
+        
+        if(s >= e) return true;
+        if(str.charAt(s) != str.charAt(e)) return false;
+
+        return isPalindrome(str, s+1, e-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        boolean ok = false;
+        ok =  isPalindrome(str,0,str.length()-1);
+        if(ok) System.out.println("YES");
+        else System.out.println("NO");
+        sc.close();
+    }
+}
